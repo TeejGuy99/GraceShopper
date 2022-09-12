f1e424bf7c7d9e00741058186ce26b01701b9d9f
TeejGuy <teejguy99@gmail.com>
2022-09-12 19:38:38 -0400

    opened API branch

--- src/api/index.js
+++ src/api/index.js
@@ -15,3 +15,62 @@ export async function getAllUsers() {
     console.error(error);
   }
 }
+
+export async function getUserInfo(userID) {
+	try {
+		return await fetch(`${BASE_URL}/user/${userID}`, {
+		  headers: {
+			"Content-Type": "application/json",
+		  },
+		})
+		  .then((response) => response.json())
+		  .then((result) => {
+			return result;
+		  });
+	  } catch (error) {
+		console.error(error);
+	  }
+}
+
+export async function logInUser({ username, password }) {
+	try {
+		return fetch(`${BASE_URL}/user/login`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				username: username,
+				password: password,
+			}),
+		})
+			.then((response) => response.json())
+			.then((result) => {
+				localStorage.setItem("token", result.token);
+				return result;
+			});
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+export async function registerUser({ username, password }) {
+	try {
+		return fetch(`${BASE_URL}/user/register`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				username: username,
+				password: password,
+			}),
+		})
+			.then((response) => response.json())
+			.then((result) => {
+				return result;
+			});
+	} catch (error) {
+		console.error(error);
+	}
+}
