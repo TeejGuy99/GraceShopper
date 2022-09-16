7f8b7939781f5d080797a99e09d9bd953dcb6e9e
AshCheeks <adelarosa2013@gmail.com>
2022-09-15 20:37:55 -0400

    styled and made the header-nav bar responsive. Still work in progress

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,76 +11,73 @@ function Header(props) {
 
   let activeClassName = "underline";
   return (
-    <>
-      <div id="header-container">
+    <div id="header-container">
+      <div className="header__content">
         <div className="logo-brand">
-          <p>Kashyyyk Candles</p>
+          <p>KASHYYYK CANDLES</p>
         </div>
         <nav>
-          <div className="nav-links">
-            <NavLink
-              to="/"
-              style={({ isActive }) => (isActive ? activeStyle : undefined)}
-            >
-              HOME
-            </NavLink>
-            <NavLink
-              to="/candles"
-              className={({ isActive }) =>
-                isActive ? activeClassName : undefined
-              }
-            >
-              CANDLES
-            </NavLink>
-            <NavLink to="/wax-melts">
-              {({ isActive }) => (
-                <span className={isActive ? activeClassName : undefined}>
-                  WAX MELTS
-                </span>
-              )}
-            </NavLink>
-            <NavLink
-              to="/all-products"
-              className={({ isActive }) =>
-                isActive ? activeClassName : undefined
-              }
-            >
-              ALL PRODUCTS
-            </NavLink>
-            {isUserAdmin ? (
-              <>
-                <NavLink
-                  to="/admin"
-                  className={({ isActive }) =>
-                    isActive ? activeClassName : undefined
-                  }
-                >
-                  Admin
-                </NavLink>
-              </>
-            ) : null}
-          </div>
-          <div className="icon-links">
-            <NavLink
-              to="/login"
-              className={({ isActive }) =>
-                isActive ? activeClassName : undefined
-              }
-            >
-              <BsPersonFill />
-            </NavLink>
-            <NavLink
-              to="/cart"
-              className={({ isActive }) =>
-                isActive ? activeClassName : undefined
-              }
-            >
-              <BsFillCartFill />
-            </NavLink>
-          </div>
+          <NavLink
+            to="/home"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            HOME
+          </NavLink>
+          <NavLink
+            to="/candles"
+            className={({ isActive }) =>
+              isActive ? activeClassName : undefined
+            }
+          >
+            CANDLES
+          </NavLink>
+          <NavLink to="/wax-melts">
+            {({ isActive }) => (
+              <span className={isActive ? activeClassName : undefined}>
+                WAX MELTS
+              </span>
+            )}
+          </NavLink>
+          <NavLink
+            to="/all-products"
+            className={({ isActive }) =>
+              isActive ? activeClassName : undefined
+            }
+          >
+            ALL PRODUCTS
+          </NavLink>
+          {isUserAdmin ? (
+            <>
+              <NavLink
+                to="/admin"
+                className={({ isActive }) =>
+                  isActive ? activeClassName : undefined
+                }
+              >
+                Admin
+              </NavLink>
+            </>
+          ) : null}
+
+          <NavLink
+            to="/login"
+            className={({ isActive }) =>
+              isActive ? activeClassName : undefined
+            }
+          >
+            <BsPersonFill size={25} />
+          </NavLink>
+          <NavLink
+            to="/cart"
+            className={({ isActive }) =>
+              isActive ? activeClassName : undefined
+            }
+          >
+            <BsFillCartFill size={25} />
+          </NavLink>
         </nav>
       </div>
-    </>
+    </div>
   );
 }
 
