b28638b377aecc79b1a56ba7342830df25dcc61f
TeejGuy <teejguy99@gmail.com>
2022-09-15 22:05:07 -0400

    saving for the night

--- src/components/App.js
+++ src/components/App.js
@@ -2,102 +2,121 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "../style/App.css";
 import { AdBanner, ItemCard, Header } from "./index";
-import { Register, AdminPage, HomePage, Login } from "../pages";
-import { getAllUsers, getReviews, getAllOrders, getAllProducts, getAllProductPhotos, logInUser, registerUser, makeUserAdmin, getAllGuests, getAllCarts } from "../api";
+import {
+	Register,
+	AdminPage,
+	HomePage,
+	Login,
+	AllProductsPage,
+} from "../pages";
+import {
+	getAllUsers,
+	getAllOrders,
+	getAllProducts,
+	getAllProductPhotos,
+	logInUser,
+	registerUser,
+	makeUserAdmin,
+	getAllGuests,
+	getAllCarts,
+} from "../api";
 
 const App = () => {
-  //UseState for various properties
-  const [isLoggedIn, setLoggedIn] = useState(false);
-  const [getUserToken, setUserToken] = useState("");
-  const [getUserCartItems, setUserCartItems] = useState([]);
-  const [isItemAvailable, setItemAvailable] = useState(true);
-  const [isUserAdmin, setUserAdmin] = useState(false);
+	//UseState for various properties
+	const [isLoggedIn, setLoggedIn] = useState(false);
+	const [getUserToken, setUserToken] = useState("");
+	const [getUserCartItems, setUserCartItems] = useState([]);
+	const [isItemAvailable, setItemAvailable] = useState(true);
+	const [isUserAdmin, setUserAdmin] = useState(false);
 
-  //Helper Functions
+	//Helper Functions
 
-  //Reset all user state on logout
-  // const resetUserStates = () => {
-  //   setUserToken(localStorage.clear());
-  //   setLoggedIn(false);
-  //   setUserCartItems([]);
-  // }
+	//Reset all user state on logout
+	// const resetUserStates = () => {
+	//   setUserToken(localStorage.clear());
+	//   setLoggedIn(false);
+	//   setUserCartItems([]);
+	// }
 
-  const runTests = () => {
-    console.log("users: ");
-    console.log(getAllUsers());
-    console.log("guests: ");
-    console.log(getAllGuests());
-    console.log("carts: ");
-    console.log(getAllCarts());
-    console.log("products: ");
-    console.log(getAllProducts());
-    // console.log("reviews: ");
-    // console.log(getReviews());
-    console.log("photos: ");
-    console.log(getAllProductPhotos());
-    console.log("orders");
-    console.log(getAllOrders());
-    console.log("attempting to sign in as tim@seed.com: ");
-    console.log(logInUser("tim@seed.com", "tim01"));
-    console.log("attempting to create a new user TeejGuy with password of password: ");
-    console.log(registerUser("TeejGuy", "password"));
-    console.log("attempting to make TeejGuy an admin: ");
-    console.log(makeUserAdmin(6));
-  }
-  useEffect(() => {
-    // runTests();
-  }, []);
+	const runTests = () => {
+		console.log("users: ");
+		console.log(getAllUsers());
+		console.log("guests: ");
+		console.log(getAllGuests());
+		console.log("carts: ");
+		console.log(getAllCarts());
+		console.log("products: ");
+		console.log(getAllProducts());
+		// console.log("reviews: ");
+		// console.log(getReviews());
+		console.log("photos: ");
+		console.log(getAllProductPhotos());
+		console.log("orders");
+		console.log(getAllOrders());
+		console.log("attempting to sign in as tim@seed.com: ");
+		console.log(logInUser("tim@seed.com", "tim01"));
+		console.log(
+			"attempting to create a new user TeejGuy with password of password: "
+		);
+		console.log(registerUser("TeejGuy", "password"));
+		console.log("attempting to make TeejGuy an admin: ");
+		console.log(makeUserAdmin(6));
+	};
 
-  return (
-    <Router>
-      <div className="app-container">
-        <Header isUserAdmin={isUserAdmin} />
-        {/* <AdBanner/> */}
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <HomePage
-                getUserToken={getUserToken}
-                isItemAvailable={isItemAvailable}
-              />
-            }
-          />
+	useEffect(() => {
+		// runTests();
+	}, []);
 
-          <Route
-            exact
-            path="/login"
-            element={
-              <Login
-                isLoggedIn={isLoggedIn}
-                setLoggedIn={setLoggedIn}
-                setUserToken={setUserToken}
-                getUserToken={getUserToken}
-                setUserAdmin={setUserAdmin}
-              />
-            }
-          />
-          <Route exact path="/register" element={<Register />} />
+	return (
+		<Router>
+			<div className="app-container">
+				<Header isUserAdmin={isUserAdmin} />
+				{/* <AdBanner/> */}
+				<Routes>
+					<Route
+						exact
+						path="/"
+						element={
+							<HomePage
+								getUserToken={getUserToken}
+								isItemAvailable={isItemAvailable}
+							/>
+						}
+					/>
+					<Route exact path="/all-products" element={<AllProductsPage />} />
+					<Route
+						exact
+						path="/login"
+						element={
+							<Login
+								isLoggedIn={isLoggedIn}
+								setLoggedIn={setLoggedIn}
+								setUserToken={setUserToken}
+								getUserToken={getUserToken}
+								setUserAdmin={setUserAdmin}
+							/>
+						}
+					/>
+					<Route exact path="/register" element={<Register />} />
 
-          <Route
-            exact
-            path="/admin"
-            element={
-              isUserAdmin ? (
-                <AdminPage
-                  isUserAdmin={isUserAdmin}
-                  getUserCartItems={getUserCartItems}
-                  isLoggedIn={isLoggedIn}
-                  setItemAvailable={setItemAvailable}
-                />
-              ) : null
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
-  );
+					<Route
+						exact
+						path="/admin"
+						element={
+							isUserAdmin ? (
+								<AdminPage
+									isUserAdmin={isUserAdmin}
+									getUserCartItems={getUserCartItems}
+									isLoggedIn={isLoggedIn}
+									setItemAvailable={setItemAvailable}
+								/>
+							) : null
+						}
+					/>
+				</Routes>
+			</div>
+		</Router>
+	);
 };
 
 export default App;
