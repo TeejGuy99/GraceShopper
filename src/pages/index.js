be88170a9cf7c5808fa2dc8d3927f32cd50f996c
Dakota Solis <65441656+INIKITS@users.noreply.github.com>
2022-09-15 19:43:59 -0500

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as HomePage } from "./HomePage";
+export { default as AdminPage } from "./AdminPage";
+export { default as Register } from "./Register";
+export { default as Login } from "./Login";
