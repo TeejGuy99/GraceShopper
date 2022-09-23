994ec8ea781bdfd10136caa66ff7473370df4f72
Teej <teejguy99@gmail.com>
2022-09-22 21:23:47 -0400

    added UI for AdminProducts page

--- src/pages/Admin-Products.js
+++ src/pages/Admin-Products.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from "react";
+import { getAllProducts } from "../api";
+import { useEffect } from "react";
+import "../style/Products.scss";
 
 export default function AdminProducts() {
+  const { products, setProducts} = props;
+  const handleRoutines = () => {
+    getAllProducts().then((results) => {
+      setProducts(results);
+    });
+  };
+  useEffect(() => {
+    handleRoutines();
+  }, []);
   return (
-    <div>Admin Products</div>
-  )
+    <div className="Products">
+      {products.map((product) => {
+        return (
+          <div className="product-items">
+            <img
+              className="item-img"
+              src={product.photos[0].link}
+              alt={product.photos[0].description}
+            />
+            <div className="item-description">
+              <p className="item-name">{product.name}</p>
+              <p className="item-price">${product.price}</p>
+            </div>
+            <div className="item-slogan">
+              <p className="slogan">{product.description}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
