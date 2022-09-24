1b116c1562d39bcb773d65665a46a692e5c216fc
Dakota Solis <dakota.h.solis@gmail.com>
2022-09-24 12:35:02 -0500

    started AdminUsers

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as RegisterForm } from "./RegisterForm";
 export { default as AdBanner } from "./AdBanner";
 export { default as ItemCard } from "./ItemCard";
 export { default as Header } from "./Header";
+export { default as AdminUsersTable } from "./AdminUsersTable"
--- src/pages/Admin-Users.js
+++ src/pages/Admin-Users.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { AdminUsersTable} from "../components"
 
 export default function AdminUsers() {
   return (
+    <>
     <div>admin Users</div>
+    < AdminUsersTable/>
+    </>
   )
 }
