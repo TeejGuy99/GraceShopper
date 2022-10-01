72cc4a03c447df5a3613395d3351479a9251ac90
Kasie <kasie.espinoza@gmail.com>
2022-09-30 22:51:20 -0500

    styling user profile

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,58 +1,67 @@
 import React, { useEffect, useState } from "react";
-import { getUserInfo, 
-  getUserOrders 
-} from "../api";
+import { getUserInfo, getUserOrders } from "../api";
 import "../style/UserProfile.scss";
 
 function UserProfile(props) {
-  const {
-    userId
-  } = props;
+  const { userId } = props;
 
-  const [user, setUser] = useState('');
+  const [user, setUser] = useState("");
   const [orders, setOrders] = useState([]);
 
   const handleRoutines = () => {
     getUserInfo(userId).then((result) => {
-      setUser(result)
-    })
+      setUser(result);
+    });
     getUserOrders(userId).then((result) => {
-      setOrders(result)
-    })
-  }
+      setOrders(result);
+    });
+  };
 
   useEffect(() => {
-    handleRoutines()
-  }, [])
-  
+    handleRoutines();
+  }, []);
 
   return (
-    <div className="userProfile" style={{display: 'flex', flexDirection: 'column', position: 'fixed', top: '20%', right: '0%'}}>
-    <div className="userProfileInfo" >
-      <h1>USER INFORMATION</h1>
-      <p>{`User ID: ${user.userId}`}</p>
-      <p>{`Email: ${user.email}`}</p>
+    <div className="profile-container">
+      <div className="profile-wrapper">
+        <h3 className="user-title">USER INFORMATION</h3>
+        <div className="user-info">
+          <div className="info">
+            <p>{`User ID: ${user.userId}`}</p>
+            <p>{`Email: ${user.email}`}</p>
+          </div>
+        </div>
+
+        <h3 className="purchase-title">PURCHASE HISTORY</h3>
+        <div className="purchase-history">
+          <div className="item-history">
+            {orders.map((order, index) => {
+              return (
+                <div key={index}>
+                  <p>{`Order: ${order.id}`}</p>
+                  {order.products.map((product, index) => {
+                    return (
+                      <div key={index}>
+                        <img className="candle-img"
+                          src={product.link}
+                          alt={product.description}
+                        />
+                        <p>
+                          {product.name}
+                        </p>
+                        <p>
+                          {`Qty: ${product.productQty}`}
+                        </p>
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="userPurchaseHistory" >
-      <h1>PURCHASE HISTORY</h1>
-      <div>{orders
-        .map((order, index) => {
-          return (
-              <div className="orders" key={index} style={{display: 'flex', width: '80vw', justifyContent: 'space-between', alignItems: 'center', margin: '10px'}}>
-                <p>{`Order: ${order.id}`}</p>
-                {order.products.map((product, index) => {
-                  return (<div key={index} style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-                    <img src={product.link} alt={product.description} style={{width: '100px'}}/>
-                    <p style={{display: 'inline'}}>{product.name}</p>
-                    <p style={{display: 'inline'}}>{`Qty: ${product.productQty}`}</p>
-                  </div>)
-                })}
-              </div>
-          );
-        })
-      }</div>
-    </div>
-  </div>
   );
 }
 
